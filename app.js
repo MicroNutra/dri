c8bbeb1eddd01c5298020c9ebdb965411870ea0d
Derek Kramer <derek.kramer88@gmail.com>
2017-06-05 16:18:21 -0600

    🔨 Fixed port bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const vitamins = require('./vitamins.js');
 const minerals = require('./minerals.js');
